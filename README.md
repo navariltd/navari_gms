acbb81eaafd65a0f958fc67db4dc8587decc0766
Lucky-Tsuma <tsumahmumba@gmail.com>
2022-10-15 22:18:28 +0300

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Grants Management System
+
+A non profit app for grants management
+
+#### License
+
+GNU General Public License (v3)
---
+++ grants_management_system/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Grants Management System",
+			"type": "module",
+			"label": _("Grants Management System")
+		}
+	]
